65c2ea02c05cd099238cd9a757b1d9b3ddc2a8dd
Jonas Francisco <102191759+jonasfrancisco@users.noreply.github.com>
2022-10-24 16:50:18 -0300

    Adicionar arquivos de projeto.

---
+++ AgendaTarefas/Models/Tarefa.cs
@@ -0,0 +1,9 @@
+﻿namespace AgendaTarefas.Models
+{
+    public class Tarefa
+    {
+        public int TarefaId { get; set; }
+        public string Nome { get; set; }
+        public string Data { get; set; }
+    }
+}
