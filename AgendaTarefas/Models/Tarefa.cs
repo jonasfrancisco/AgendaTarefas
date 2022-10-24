c364c1fe357c6720d3bd007e143ff6fce7b92ee8
Jonas Francisco <102191759+jonasfrancisco@users.noreply.github.com>
2022-10-24 16:58:14 -0300

    Criação Model Tarefa

--- AgendaTarefas/Models/Tarefa.cs
+++ AgendaTarefas/Models/Tarefa.cs
@@ -1,9 +1,17 @@
-﻿namespace AgendaTarefas.Models
+﻿using System.ComponentModel.DataAnnotations;
+
+namespace AgendaTarefas.Models
 {
     public class Tarefa
     {
         public int TarefaId { get; set; }
+        [Required(ErrorMessage ="{0} é obrigatório")]
+        [StringLength(50,ErrorMessage ="Use menos caracteres")]
+
         public string Nome { get; set; }
         public string Data { get; set; }
+        [Required(ErrorMessage = "{0} é obrigatório")]
+        [DataType(DataType.Time)]
+        public string Horario { get; set; }
     }
 }
