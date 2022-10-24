a0c3df0e288d1b6956a906c85f72084aa8eba173
Jonas Francisco <102191759+jonasfrancisco@users.noreply.github.com>
2022-10-24 17:46:57 -0300

    Criando Controller e datas

---
+++ AgendaTarefas/Controllers/TarefasController.cs
@@ -0,0 +1,41 @@
+﻿using AgendaTarefas.Models;
+using Microsoft.AspNetCore.Mvc;
+using System;
+using System.Collections.Generic;
+
+namespace AgendaTarefas.Controllers
+{
+    public class TarefasController : Controller
+    {
+        private readonly Contexto _Contexto;
+        public TarefasController(Contexto contexto)
+        {
+            _Contexto = contexto;
+        }
+        public IActionResult Index()
+        {
+            return View(PegarDatas());
+        }
+
+        private List<DatasViewModel> PegarDatas()
+        {
+            DateTime dataAtual = DateTime.Now;
+            DateTime dataLimite = DateTime.Now.AddDays(10);
+            int qtdDias = 0;
+            DatasViewModel data;
+           List<DatasViewModel> ListaDatas = new List<DatasViewModel>();
+
+            while(dataAtual < dataLimite)
+            {
+                data = new DatasViewModel();
+                data.Data = dataAtual.ToShortDateString();
+                data.Identificadores = "colapse" + dataAtual.ToShortDateString().Replace("/", "");
+                ListaDatas.Add(data);
+                qtdDias = qtdDias + 1;
+                dataAtual = dataAtual.AddDays(qtdDias);
+                
+            }
+            return ListaDatas;
+        }
+    }
+}
---
+++ AgendaTarefas/Models/DatasViewModel.cs
@@ -0,0 +1,8 @@
+﻿namespace AgendaTarefas.Models
+{
+    public class DatasViewModel
+    {
+        public string Data { get; set; }
+        public string Identificadores { get; set; }
+    }
+}
